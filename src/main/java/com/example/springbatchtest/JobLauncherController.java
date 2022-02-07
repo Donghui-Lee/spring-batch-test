3dc6cd2d2644ba40a8db7089093c968cf1ac654e
Donghui-Lee <ehdgml3206@naver.com>
2022-02-07 11:58:12 +0900

    feat: 비동기 방식 추가

--- src/main/java/com/example/springbatchtest/JobLauncherController.java
+++ src/main/java/com/example/springbatchtest/JobLauncherController.java
@@ -7,10 +7,13 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +27,9 @@ public class JobLauncherController {
     @Autowired
     private JobLauncher jobLauncher;
 
+    @Autowired
+    private BasicBatchConfigurer basicBatchConfigurer;
+
     @PostMapping("batch")
     public String launch(@RequestBody Member member) throws
         JobParametersInvalidException,
@@ -36,8 +42,13 @@ public class JobLauncherController {
             .addDate("date", new Date())
             .toJobParameters();
 
+        // 비동기 방식
+        SimpleJobLauncher jobLauncher = (SimpleJobLauncher) basicBatchConfigurer.getJobLauncher();
+        jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
         jobLauncher.run(job, jobParameters);
 
+        // 동기 방식
+        // this.jobLauncher.run(job, jobParameters);
         return "Batch Completed";
     }
 }
